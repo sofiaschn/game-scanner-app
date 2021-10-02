a4f8278ceae8d0f3cd087dd2c09f0c2044e51aaf
Sofia Schneider <sofia.schn.c@gmail.com>
2021-10-02 01:42:27 -0300

    Fix bug on snake game and add random spawn

--- src/game/game.js
+++ src/game/game.js
@@ -1,30 +1,23 @@
-let ctx = canvas.getContext('2d');
-let snake = [[8, 8]],
-    apple = [4, 4],
+let ctx = canvas.getContext('2d'),
+    random = () => [Math.floor(Math.random() * 16),Math.floor(Math.random() * 16)],
+    snake = [random()],
+    apple = random(),
     [dx, dy] = [0, 0];
 
 setInterval(() => {
-    switch (direction) {
-        case 'up':
-            [dx, dy] = [0, -1];
-            break;
-        case 'down':
-            [dx, dy] = [0, 1];
-            break;
-        case 'left':
-            [dx, dy] = [-1, 0];
-            break;
-        case 'right':
-            [dx, dy] = [1, 0];
-            break;
+    if (direction == 'up' && dy !== 1) {
+      [dx, dy] = [0, -1];
+    } else if (direction == 'down' && dy !== -1) {
+      [dx, dy] = [0, 1];
+    } else if (direction == 'left' && dx !== 1) {
+      [dx, dy] = [-1, 0];
+    } else if (direction == 'right' && dx !== -1) {
+      [dx, dy] = [1, 0];
     }
     snake.unshift([(snake[0][0] + dx) & 15, (snake[0][1] + dy) & 15]);
     if ('' + snake[0] == apple) {
         do {
-            apple = [
-                Math.floor(Math.random() * 16),
-                Math.floor(Math.random() * 16),
-            ];
+            apple = random();
         } while (snake.some((seg) => '' + seg == apple));
     } else if (snake.slice(1).some((seg) => '' + seg == snake[0])) {
         snake.splice(1);
@@ -37,4 +30,4 @@ setInterval(() => {
     ctx.fillRect(apple[0] * 16, apple[1] * 16, 16, 16);
     ctx.fillStyle = 'lime';
     snake.forEach(([x, y]) => ctx.fillRect(x * 16, y * 16, 16, 16));
-}, 125);
+}, 250);
